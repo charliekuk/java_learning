9d5a3a176e89fca30386e676e6d5ab09ece56d9b
cmgu <376872037@qq.com>
2017-02-07 22:37:01 +0800

    javalearning测试程序
    读书时候的联系工程（乱七八糟）

---
+++ src/XCountToZero.java
@@ -0,0 +1,13 @@
+/**
+ * Created by chaominggu on 2017/2/7.
+ */
+public class XCountToZero {
+    public static void main(String[] args) {
+        int x = 99;
+        while(x>=0){
+            System.out.println(x);
+            x=x-1;
+        }
+        System.out.println("Counting is over!");
+    }
+}
